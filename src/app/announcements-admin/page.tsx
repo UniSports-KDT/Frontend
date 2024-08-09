8e6e318f6ccccc61b64dbbe9c02978c3709d9998
이정훈 <niko36@naver.com>
2024-08-09 16:40:56 +0900

    announcements-admin(공지사항 관리) 추가완료

---
+++ src/app/announcements-admin/page.tsx
@@ -0,0 +1,8 @@
+import { AnnouncementsAdmin } from '@/components/component/announcements-admin';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <AnnouncementsAdmin/>
+    );
+  }
