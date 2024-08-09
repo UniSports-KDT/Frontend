165a35d9fb520c77243b67a3525adc36ac03bfe5
이정훈 <niko36@naver.com>
2024-08-09 16:33:21 +0900

    facility-edit 수정 후  추가완료

---
+++ src/app/facility-edit/page.tsx
@@ -0,0 +1,8 @@
+import { FacilityEdit } from '@/components/component/facility-edit';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <FacilityEdit/>
+    );
+  }
