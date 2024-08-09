fe4c5ccade8a7a045c2bf1ed0eaa278fef7a7dc2
이정훈 <niko36@naver.com>
2024-08-09 16:47:53 +0900

    facility-create(새로운 시설 등록) 추가완료

---
+++ src/app/facility-create/page.tsx
@@ -0,0 +1,8 @@
+import { FacilityCreate } from '@/components/component/facility-create';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <FacilityCreate/>
+    );
+  }
