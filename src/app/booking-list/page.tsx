a7db75fb30a27d90f78ac211898313cd55df7df9
이정훈 <niko36@naver.com>
2024-08-09 15:48:26 +0900

    booking-list(예약 내역) 추가완료

---
+++ src/app/booking-list/page.tsx
@@ -0,0 +1,8 @@
+import { BookingList } from '@/components/component/booking-list';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <BookingList/>
+    );
+  }
