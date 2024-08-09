e3e9f3edb09d5bef3149761cb54d6763a27917e9
이정훈 <niko36@naver.com>
2024-08-09 16:44:32 +0900

    announcement-write(공지사항 작성) 추가완료

---
+++ src/app/announcement-write/page.tsx
@@ -0,0 +1,8 @@
+import { AnnouncementWrite } from '@/components/component/announcement-write';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <AnnouncementWrite/>
+    );
+  }
