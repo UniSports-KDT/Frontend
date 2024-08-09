e194c120f63b7a3605e1041986a6f5639815cda3
이정훈 <niko36@naver.com>
2024-08-09 15:36:23 +0900

    facility-list(시설목록) 추가완료

---
+++ src/app/facility-list/page.tsx
@@ -0,0 +1,8 @@
+import { FacilityList } from '@/components/component/facility-lists';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <FacilityList/>
+    );
+  }
